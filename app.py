e267a46e32ac1813326513d40408e3a3958418bc
Chanchati Varsha <varshasagar673@gmail.com>
2025-08-10 11:52:26 +0530

    intial commit

--- app.py
+++ app.py
@@ -53,5 +53,7 @@ def chat():
         response = f"Error: {e}"
     return jsonify({"response": response})
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
